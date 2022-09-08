65c875bf770f63ac1148bf7f753a9d9236edf0ac
Erik Merkle <erik.merkle@datastax.com>
2022-09-08 10:31:21 -0500

    Fix keyspace example

--- examples/advanced/keyspace_workflow/resource.tf
+++ examples/advanced/keyspace_workflow/resource.tf
@@ -10,18 +10,53 @@ resource "astra_database" "db1" {
   keyspace       = "myks"
 }
 
-resource "astra_role" "role1" {
-    role_name = "role1"
+resource "astra_keyspace" "appks1" {
+  name = "appks1"
+  database_id = astra_database.db1.id
+}
+
+resource "astra_keyspace" "appks2" {
+  name = "appks2"
+  database_id = astra_database.db1.id
+}
+
+resource "astra_keyspace" "appks3" {
+  name = "appks3"
+  database_id = astra_database.db1.id
+}
+
+resource "astra_role" "alldbsrole" {
+    role_name = "alldbsrole"
+    description = "role description"
+    effect = "allow"
+    resources = [
+                  "drn:astra:org:<org id>:db:*",
+                  "drn:astra:org:<org id>:db:*:keyspace:*",
+                  "drn:astra:org:<org id>:db:*:keyspace:*:table:*",
+    ]
+    policy = [
+               # "org-db-view" is required to list databases
+               "org-db-view",
+               # the following are for CQl and table operations
+               "db-cql", "db-table-alter", "db-table-create", "db-table-describe", "db-table-modify", "db-table-select",
+               # the following are for Keysapce operations
+               "db-keyspace-alter", "db-keyspace-describe", "db-keyspace-modify", "db-keyspace-authorize", "db-keyspace-drop", "db-keyspace-create", "db-keyspace-grant",
+    ]
+}
+
+resource "astra_role" "selectkeyspacesrole" {
+    role_name = "selectkeyspacesrole"
     description = "role description"
     effect = "allow"
     resources = [
                   "drn:astra:org:<org id>:db:${astra_database.db1.id}",
-                  # the following line grants permissions to ALL keyspaces within an org
-                  "drn:astra:org:<org id>:db:${astra_database.db1.id}:keyspace:*",
-                  # if you want to grant permissions only to specific keyspaces, do not use the above line,
-                  # instead specify each keyspace. NOTE: The keyspaces below do not have to exist at this point.
-                  "drn:astra:org:<org id>:db:${astra_database.db1.id}:keyspace:app1keyspace",
-                  "drn:astra:org:<org id>:db:${astra_database.db1.id}:keyspace:app2keyspace",
+                  # NOTE: The keyspaces below do not have to exist when this role is created
+                  "drn:astra:org:<org id>:db:${astra_database.db1.id}:keyspace:appks1",
+                  "drn:astra:org:<org id>:db:${astra_database.db1.id}:keyspace:appks1:table:*",
+                  "drn:astra:org:<org id>:db:${astra_database.db1.id}:keyspace:appks3",
+                  "drn:astra:org:<org id>:db:${astra_database.db1.id}:keyspace:appks3:table:*",
+                  "drn:astra:org:<org id>:db:${astra_database.db1.id}:keyspace:notcreatedappks",
+                  "drn:astra:org:<org id>:db:${astra_database.db1.id}:keyspace:notcreatedappks:table:*",
     ]
     policy = [
                # "org-db-view" is required to list databases
