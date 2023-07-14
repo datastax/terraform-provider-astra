dc8147d8930ea3a0e662e7bd27e196c52fdb4b21
David Dieruf <david@dierufventures.com>
2023-07-14 16:51:49 -0400

    Improved formatting of registry home page

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -7,9 +7,18 @@ terraform {
   }
 }
 
-variable "token" {}
+# Create a new Astra database
+resource "astra_database" "example" {
+  name           = "name"
+  keyspace       = "keyspace"
+  cloud_provider = "gcp"
+  regions        = ["us-east1"]
+}
 
-provider "astra" {
-  // This can also be set via ASTRA_API_TOKEN environment variable.
-  token = var.token
+# Create a new Astra streaming tenant
+resource "astra_streaming_tenant" "example_tenant" {
+  tenant_name    = "mystreamingtenant1"
+  cloud_provider = "gcp"
+  region         = "useast-1"
+  user_email     = "someuser@example.com"
 }
