6b57f67505d9985a19c53338849f7972a6e5105e
Paul Gier <paul.gier@datastax.com>
2023-07-20 12:53:04 -0500

    run terraform fmt during CI
    - run terraform fmt against the ./examples directory during CI
    - check for any diffs compared to latest commit
    - fix deprecation warning related to the 'set-output' command

--- main.go
+++ main.go
@@ -14,13 +14,23 @@ import (
 
 const providerName = "datastax/astra"
 
+// Run "go generate" to format example terraform files and generate the docs for the registry/website
+
+// If you do not have terraform installed, you can remove the formatting command, but its suggested to
+// ensure the documentation is formatted properly.
+//go:generate terraform fmt -recursive ./examples/
+
+// Run the docs generation tool, check its repository for more information on how it works and how docs
+// can be customized.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 var (
 	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary
+	// to appropriate values for the compiled binary.
 	version string = "dev"
 
-	// goreleaser can also pass the specific commit if you want
-	// commit  string = ""
+	// goreleaser can pass other information to the main package, such as the specific commit
+	// https://goreleaser.com/cookbooks/using-main.version/
 )
 
 func main() {
