d3d2ceef002577067bfb7a84e3609a5057b8e8e4
Paul Gier <paul.gier@datastax.com>
2024-02-23 11:44:10 -0600

    fix astra db table import (#368)
    The table import feature was not working due to missing the database
    region in the importID
    Fixes #366

--- examples/resources/astra_table/import.sh
+++ examples/resources/astra_table/import.sh
@@ -1,2 +1,2 @@
 # the import id includes the database_id, keyspace name, and table name.
-terraform import astra_table.example 48bfc13b-c1a5-48db-b70f-b6ef9709872b/kayspacename/tablename
+terraform import astra_table.example 48bfc13b-c1a5-48db-b70f-b6ef9709872b/us-central1/keyspacename/tablename
