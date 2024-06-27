55b6c5d49faee29e3097156a12524df973d33629
Erik Merkle <erik.merkle@datastax.com>
2024-06-26 23:08:53 -0500

    Add Enterprise Organization operations (#398)

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.21
 toolchain go1.22.0
 
 require (
-	github.com/datastax/astra-client-go/v2 v2.2.54
+	github.com/datastax/astra-client-go/v2 v2.2.57
 	github.com/datastax/pulsar-admin-client-go v0.0.0-20230707040954-1a4745e07587
 	github.com/google/uuid v1.6.0
 	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320
---
+++ internal/provider/resource_enterprise_org.go
@@ -0,0 +1,140 @@
+package provider
+
+import (
+	"context"
+	"errors"
+
+	"github.com/datastax/astra-client-go/v2/astra"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceEnterpriseOrg() *schema.Resource {
+	return &schema.Resource{
+		Description:   "`enterprise_org` resource represents an Organization that is created under an Enterprise in Astra.",
+		CreateContext: resourceEnterpriseOrgCreate,
+		ReadContext:   resourceEnterpriseOrgRead,
+		DeleteContext: resourceEnterpriseOrgDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			// Required
+			"name": {
+				Description: "Organization name",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+			"email": {
+				Description: "Organization email address",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+			"admin_user_id": {
+				Description: "UUID of the Astra user that will be the admin of the organization",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+			// Computed
+			"enterprise_id": {
+				Description: "UUID of the Enterprise under which the organization is created",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"organization_id": {
+				Description: "The Astra organization ID (UUID) for the created Enterprise organization.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"organization_type": {
+				Description: "The type of the organization.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"organization_group_id": {
+				Description: "The group ID (UUID) of the organization.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"created_at": {
+				Description: "The timestamp when the organization was created.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"last_modified": {
+				Description: "The timestamp when the organization was last modified.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			// TODO: Add MarketPlaceData
+		},
+	}
+}
+
+func resourceEnterpriseOrgCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(astraClients).astraClient.(*astra.ClientWithResponses)
+
+	orgName := d.Get("name").(string)
+	orgEmail := d.Get("email").(string)
+	adminUid := d.Get("admin_user_id").(string)
+
+	orgReq := astra.CreateOrganizationInEnterpriseJSONRequestBody{
+		Name:         orgName,
+		Email:        orgEmail,
+		AdminUserID:  adminUid,
+	}
+
+	resp, err := client.CreateOrganizationInEnterpriseWithResponse(ctx, orgReq)
+	if err != nil {
+		return diag.FromErr(err)
+	} else if resp.StatusCode() != 201 {
+		return diag.Errorf("error adding Organization to Enterprise: Status: %s, %s", resp.Status(), resp.Body)
+	}
+
+	enterpriseOrg := resp.JSON201
+
+	d.SetId(*enterpriseOrg.OrganizationID)
+	if err := setEnterpriseOrgData(d, enterpriseOrg); err != nil {
+		return diag.FromErr(err)
+	}
+	return nil
+}
+
+func resourceEnterpriseOrgRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return diag.Diagnostics{
+		diag.Diagnostic{
+			Severity: diag.Warning,
+			Summary:  "Read of Enterprise Organizations not supported",
+			Detail:  "Read of Enterprise Organizations not supported.",
+		},
+	}
+}
+
+func resourceEnterpriseOrgDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return diag.Diagnostics{
+		diag.Diagnostic{
+			Severity: diag.Warning,
+			Summary:  "Delete of Enterprise Organizations not supported",
+			Detail:  "Delete of Enterprise Organizations not supported.",
+		},
+	}
+}
+
+func setEnterpriseOrgData(d *schema.ResourceData, org *astra.CreateOrgInEnterpriseResponse) error {
+	if org == nil {
+		return errors.New("organization is nil")
+	}
+	d.Set("enterprise_id", *org.EnterpriseId)
+	d.Set("organization_id", *org.OrganizationID)
+	d.Set("organization_type", *org.OrgType)
+	d.Set("organization_group_id", *org.OrganizationGroupId)
+	d.Set("created_at", *org.CreatedAt)
+	d.Set("last_modified", *org.LastModified)
+
+	return nil
+}
