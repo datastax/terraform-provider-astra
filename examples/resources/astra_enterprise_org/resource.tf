6064dfc9781d0e703fea90dc23fd895528cd75f2
Erik Merkle <erik.merkle@datastax.com>
2025-02-14 10:22:52 -0600

    Update docs for astra_enterprise_org
    This patch updates the registry docs to indicate that
    import is not currently supported for astra_enterprise_org
    objects as there is no READ operation currently in the
    DevOps API

--- examples/resources/astra_enterprise_org/resource.tf
+++ examples/resources/astra_enterprise_org/resource.tf
@@ -3,5 +3,4 @@ resource "astra_enterprise_org" "entorg" {
   name          = "My Enterprise Organization"
   email         = "admin@example.com"
   admin_user_id = "a1b2c3d4-5e6f-7a8b-9c0d-1e2f3a4b5c6d"
-  enterprise_id = "a1b2c3d4-5e6f-7a8b-9c0d-1e2f3a4b5c6d"
 }
