1ba2078e4a1206b60ae54d244c1b88653177171a
Brad Dunn <brad.dunn@datastax.com>
2025-11-21 13:17:03 -0500

    fix(docs): typo in pcu group association documentation

--- examples/resources/astra_pcu_group_association/resource.tf
+++ examples/resources/astra_pcu_group_association/resource.tf
@@ -7,6 +7,6 @@ resource "astra_pcu_group" "pcu_group" {
 }
 
 resource "astra_pcu_group_association" "assoc" {
-  pcu_group_id  = data.astra_pcu_group.pcu_group.id
-  datacenter_id = provider::astra::resolve_datacenter(data.astra_database.db, "us-west-2")
+  pcu_group_id  = astra_pcu_group.pcu_group.id
+  datacenter_id = provider::astra::resolve_datacenter(astra_database.db, "us-west-2")
 }
